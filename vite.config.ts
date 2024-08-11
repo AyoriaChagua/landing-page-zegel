78cd6bf8a1b7e7f86f98cf0a9f9a3c8b5388173b
AyoriaChagua <ayoriachagua@gmail.com>
2024-08-11 12:27:00 -0500

    fix: add deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing-page-zegel/'
+  base: '/landing-page-zegel/',
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(process.env.VITE_SUPABASE_KEY),
+  },
 })
